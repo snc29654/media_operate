6f1d60351b0222850977820995f10ab73b120245
snc29654 <snc29654@nifty.com>
2021-10-18 14:13:34 +0900

    画像アップロードする　しない　メニュー変更

--- index.php
+++ index.php
@@ -241,8 +241,8 @@ echo '</p>';
                         echo "<input type=\"submit\" value=\"アップロード\">";
 
                         echo "<select name=\"fileact\" id=\"fileact\">";
-                        echo "<option value=\"fileup\">アップロードする</option>";
-                        echo "<option value=\"filenoup\">アップロードしない</option>";
+                        echo "<option value=\"fileup\">画像アップロードする</option>";
+                        echo "<option value=\"filenoup\">画像アップロードしない</option>";
                         echo "</select></p>";
                         echo "<form>";
                         echo "<action=\"index.php\" enctype=\"multipart/form-data\" method=\"post\">";
